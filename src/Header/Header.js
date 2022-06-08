8461159b989050c31e60cd96c947bc06acf969b0
kathrynzaro <zarokathryn@gmail.com>
2022-06-07 17:41:15 -0700

    adding minor styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Header.css';
 
 export default function Header(props) {
-  return <header>Alchemy Animal Farm, by {props.name}</header>;
+  return <h1>Alchemy Animal Farm, by {props.name}</h1>;
 }
